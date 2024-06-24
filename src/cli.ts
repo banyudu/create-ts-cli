f4a911b4c0a720212836a7e7ecacfb380a6e9c02
banyudu <banyudu@gmail.com>
2024-06-24 08:14:50 +0800

    init

---
+++ src/cli.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env tsx
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import main, { Options } from '.'
+
+(async () => {
+  const argv = await yargs(hideBin(process.argv)).parse()
+  const options: Options = argv
+  await main(options)
+})().catch(console.error)
+
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export interface Options {
+}
+
+export default async function main(options: Options) {
+  console.log('Hello, cli! options:', options)
+}
